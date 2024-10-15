880e4fdedb696fb8a11cec57fedcdcbcbf38bc8b
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-15 22:28:32 +0300

    Create PasswordController & PasswordOptiosModel

---
+++ PasswordGenerator/PasswordGenerator/Controllers/PasswordController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Models;
+using System.Diagnostics;
+
+namespace PasswordGenerator.Controllers
+{
+    public class PasswordController : Controller
+    {
+        private readonly ILogger<PasswordController> _logger;
+
+        public PasswordController(ILogger<PasswordController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GeneratePassword(PasswordOptionsModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGenerator/Models/PasswordOptionsModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordGenerator.Models
+{
+    public class PasswordOptionsModel
+    {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(20)]
+        public int PasswordLength { get; set; }
+        public bool IncludeUppercaseLetters { get; set; }
+        public bool IncludeLowercaseLetters { get; set; }
+        public bool IncludeNumbers { get; set; }
+        public bool IncludeSymbols { get; set; }
+
+    }
+}
