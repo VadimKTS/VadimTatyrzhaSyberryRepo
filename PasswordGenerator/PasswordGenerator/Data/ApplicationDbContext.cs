b30fd7db7531d1783f304ea40a258369614918ed
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-16 14:39:29 +0300

    add DbContext

---
+++ PasswordGenerator/PasswordGenerator/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordGenerator.Models;
+
+namespace PasswordGenerator.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<BookModel> Books { get; set; }
+    }
+}
