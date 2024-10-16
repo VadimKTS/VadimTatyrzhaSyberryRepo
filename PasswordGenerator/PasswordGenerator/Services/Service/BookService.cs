4cb792dfe4eb03ebf77cbe7487a4e1c00f5e5fd2
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-16 14:51:24 +0300

    add BookService

---
+++ PasswordGenerator/PasswordGenerator/Services/Service/BookService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordGenerator.Data;
+using PasswordGenerator.Models;
+using PasswordGenerator.Services.Interface;
+
+namespace PasswordGenerator.Services.Service
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<BookModel>> SearchBooksAsync(string query)
+        {
+            return await _context.Books
+                                 .Where(b => b.Title.Contains(query) || b.Author.Contains(query))
+                                 .ToListAsync();
+        }
+    }
+}
