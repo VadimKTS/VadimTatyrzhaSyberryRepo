dc4a2fd26423dc644c3254d8da709f527e7328d7
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-16 11:49:16 +0300

    add CurrencyServise to controller

--- PasswordGenerator/PasswordGenerator/Controllers/CurrencyConverterController.cs
+++ PasswordGenerator/PasswordGenerator/Controllers/CurrencyConverterController.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Services.Interface;
 
 namespace PasswordGenerator.Controllers
 {
     public class CurrencyConverterController : Controller
     {
+        private readonly ICurrencyService _currencyService;
+
+        public CurrencyConverterController(ICurrencyService currencyService)
+        {
+            _currencyService = currencyService;
+
+        }
+
+
     }
 }
