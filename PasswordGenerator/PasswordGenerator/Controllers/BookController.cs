f69b4ef3ea157244cefd9f89a6280a30c2a404cf
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-16 14:52:38 +0300

    Create BookController.cs

---
+++ PasswordGenerator/PasswordGenerator/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Models;
+using System.Diagnostics;
+
+namespace PasswordGenerator.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+                      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
