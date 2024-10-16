12f0378db37c14119fb618db5d24ff47cf37ba16
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-16 15:19:34 +0300

    add defaut data

--- PasswordGenerator/PasswordGenerator/Data/ApplicationDbContext.cs
+++ PasswordGenerator/PasswordGenerator/Data/ApplicationDbContext.cs
@@ -8,5 +8,18 @@ namespace PasswordGenerator.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<BookModel> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BookModel>().HasData(
+                new BookModel { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", PublishedDate = "1925", Picture = "default" },
+                new BookModel { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", PublishedDate = "1960", Picture = "default" },
+                new BookModel { Id = 3, Title = "1984", Author = "George Orwell", PublishedDate = "1949", Picture = "default" },
+                new BookModel { Id = 4, Title = "The Master and Margarita", Author = "Mikhail Bulgakov", PublishedDate = "1940", Picture = "default" },
+                new BookModel { Id = 5, Title = "The Lord of the Rings", Author = "John Ronald Reuel Tolkien", PublishedDate = "1949", Picture = "default" }
+            );
+        }
     }
 }
