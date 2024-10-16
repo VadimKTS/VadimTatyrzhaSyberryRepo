8eb295f609bea3cd73f4aea9f6c3224aa6f601cc
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-16 11:43:59 +0300

    create interface ICurrencyService

---
+++ PasswordGenerator/PasswordGenerator/Models/CurrencyConverterModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PasswordGenerator.Models
+{
+    public class CurrencyConverterModel
+    {
+        public decimal Amount { get; set; }
+        public string SourceCurrency { get; set; }
+        public string DestinationCurrency { get; set; }
+        public decimal ConvertedAmount { get; set; }
+        public IEnumerable<SelectListItem> Currencies { get; set; }
+    }
+}
