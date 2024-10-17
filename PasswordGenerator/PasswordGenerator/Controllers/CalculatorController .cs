a0d0a43da5615c002e0dd134e0243b94a1d2c8b6
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-17 12:00:16 +0300

    Create CalculatorController .cs

---
+++ PasswordGenerator/PasswordGenerator/Controllers/CalculatorController .cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Models;
+using System.Diagnostics;
+
+namespace PasswordGenerator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(CalculatorModel calculatorModel)
+        {
+            try
+            {
+                //calculate logic
+
+            }
+            catch (Exception ex)
+            {
+                calculatorModel.Error = ex.Message;
+            }
+            return View("Calculator", calculatorModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
