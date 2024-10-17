492eaf963196c7c0d6319f9f3857b010ff48db9a
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2024-10-17 11:55:03 +0300

    Create CalculatorModel.cs

---
+++ PasswordGenerator/PasswordGenerator/Models/CalculatorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PasswordGenerator.Models
+{
+    public class CalculatorModel
+    {
+        public string Operation { get; set; }
+        public decimal Number1 { get; set; }
+        public decimal Number2 { get; set; }
+        public decimal Result { get; set; }
+        public string Error { get; set; } = string.Empty;
+    }
+}
